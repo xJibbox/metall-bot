b2a42f48961e10e17e02a9d570f4f1952b238cb6
Luis <luisklingbeil2006@gmail.com>
2026-02-13 17:23:44 +0100

    Added web server for Render

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot läuft!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Webserver läuft.");
+});
 const { Client, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
 
